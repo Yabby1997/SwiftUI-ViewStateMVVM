c7cd497c5fa64c62c61467a239b7a472b9ef94af
Yabby1997 <yabby1997@gmail.com>
2022-10-02 15:54:39 +0900

    initial commit

---
+++ ViewStateMVVM/ViewStateMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewStateMVVMApp.swift
+//  ViewStateMVVM
+//
+//  Created by USER on 2022/10/02.
+//
+
+import SwiftUI
+
+@main
+struct ViewStateMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
